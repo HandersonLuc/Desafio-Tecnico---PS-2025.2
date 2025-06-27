71ce92f516c87c1d58e0c553853d8e424b8987d1
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-06-27 19:00:55 -0300

    feat: add index.html and arquivo.js

---
+++ Estudo/arquivo.js
@@ -0,0 +1,25 @@
+//Declaração de variáveis com var, let e const
+let vari; // Variável de escopo de bloco
+
+// tipos de dados primitivos: number, string, boolean, null, undefined
+
+
+// JavaScript como linguagem de tipagem dinâmica
+
+//  Nomeação de variaveis: regras e padronização camelCase
+
+let nomeCompleto = "João da Silva"; // camelCase
+function funcaoJavaScript() {
+    console.log("Função JavaScript");
+}
+
+//Recursos de navegador: console.log, alert, prompt e confirm
+vari = confirm("Alerta!!!");
+console.log(`Usuario dogotou isso: ${vari}`);
+//console.log(`Numero = ${vari}`);
+
+// operadores aritméticos: +, -, *, /, %, ++, --
+let num1 = 10;
+let num2 = 5;
+console.log(`Soma: ${num1 + num2}`); // Exemplo de soma
+let soma = num1 + num2;
